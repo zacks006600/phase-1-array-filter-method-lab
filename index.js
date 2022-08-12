35a5dd36e05c28f2b5c34c6f3308ee64a1e0fe08
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-08-12 18:32:22 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, lookFor ){
+    return drivers.filter(nameOfItem => nameOfItem.toLowerCase() === lookFor.toLowerCase() )
+}
+function fuzzyMatch(drivers, lookFor){
+    return drivers.filter(nameOfItem => nameOfItem.startsWith(lookFor) )
+}
+function matchName(drivers,lookFor){
+    {
+        return drivers.filter(nameOfItem => nameOfItem.name === lookFor)
+    }
+} 
